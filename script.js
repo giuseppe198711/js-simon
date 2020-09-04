2ad60fe22a3156050bfe6ff06e1aa3463f88cbcf
Giuseppe <giuseppedf8711@gmail.com>
2020-09-04 17:22:06 +0200

    aggiunta 1

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
-$(document).ready(function() {
-//   Un alert espone 5 numeri casuali diversi.
+// Un alert espone 5 numeri casuali diversi.
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
 // Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
 
 
-// genero 5 numeri casuali con un alert
 
-// dopo 30 secondi l utente deve inserire in 5 prompt diversi i numeri che erano comparsi prima
+// funzione che genera un numero casuale tra 1 e 1000
+function numeroRandom() { //lasciamo le parentesi vuote perche la funzione non accetta paramentri in entrata
+  var numeroACaso = Math.floor(Math.random() * 1000) + 1;
+  return numeroACaso;
+}
 
+var numeroAcaso = numeroRandom();
 
-//  quando l'utente ha inserito i 5 numeri si andranno a confrontare i numeri dell'utente con quelli comparsi all'inizio
+var arrayNumUtente = []
 
+// sono in questo caso i 30' che l'utente deve aspettare per inserire i numeri che ha memorizzato
+setTimeout(myFunction,30000);
 
-
-
-
-
-
-
-};
+// l'utente deve inserire i numeri che ha appena visto
+function myFunction() {
+  var numeroUtente = parseInt(prompt("Inserisci i numeri che hai appena visto"));
+}
