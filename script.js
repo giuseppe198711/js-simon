9d2034e843de235736a37a8c7085d61003b6753e
Giuseppe <giuseppedf8711@gmail.com>
2020-09-04 15:49:08 +0200

    inizio -come procedo

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+//   Un alert espone 5 numeri casuali diversi.
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+// genero 5 numeri casuali con un alert
+
+// dopo 30 secondi l utente deve inserire in 5 prompt diversi i numeri che erano comparsi prima
+
+
+//  quando l'utente ha inserito i 5 numeri si andranno a confrontare i numeri dell'utente con quelli comparsi all'inizio
+
+
+
+
+
+
+
+
+};
